b671bbee1c7f8096592f402a9fed3915e4ab300c
AlexSytsenko <sytsenko@gmail.com>
2021-03-19 11:31:50 +0200

    Updates

---
+++ static/js/main.14df136b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),s=c.n(o),i=c(0);var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Hello fro react"})})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.14df136b.chunk.js.map
