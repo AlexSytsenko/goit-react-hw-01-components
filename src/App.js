cfc12a1cad5478101c05ecbbe0039f3a0d290c4e
AlexSytsenko <sytsenko@gmail.com>
2021-03-19 18:25:20 +0200

    add Statistics component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Profile from './components/Profile';
+import Statistics from './components/Statistics';
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 function App() {
   return (
+
+  <div>
     <Profile
       name={user.name}
       tag={user.tag}
@@ -10,6 +14,14 @@ function App() {
       location={user.location}
       stats={user.stats}
     />
+    
+    <Statistics 
+      title = "Upload stats"
+      stats = { statisticalData }
+    />
+      
+  </div>
+      
 
   )
 }
